4f73758e7b92f9601050085da65f6948998c847b
Laurent Carrie <laurent.carrie@bayer.com>
2023-06-09 15:57:58 +0200

    almost

--- src/storylc/cli.py
+++ src/storylc/cli.py
@@ -9,12 +9,11 @@ import pluto
 import yaml
 from dacite import from_dict  # type:ignore
 from jinja2 import Environment, PackageLoader, select_autoescape  # type:ignore
-from storylc.model import Movie
-from storylc.project_logs import init_logs, a_logger
 from storylc.generate import generate
+from storylc.model import Movie
+from storylc.project_logs import a_logger, init_logs
 
 here = Path(__file__).absolute().parent
-
 
 
 @click.group()
@@ -32,9 +31,9 @@ def _generate(out: str, yml: str):
         yml_path = Path(yml)
         data = yaml.load(yml_path.read_text(), yaml.FullLoader)
         movie: Movie = from_dict(data_class=Movie, data=data)
-        movie = Movie(root=yml_path.parent,scenes=movie.scenes)
+        movie = Movie(root=yml_path.parent, scenes=movie.scenes)
         a_logger.info(movie)
-        generate(movie=movie,out=Path(out))
+        generate(movie=movie, out=Path(out))
     except:  # noqa:E722
         traceback.print_exc()
         sys.exit(1)
--- src/storylc/generate.py
+++ src/storylc/generate.py
@@ -1,5 +1,6 @@
 from functools import reduce
 from pathlib import Path
+from typing import List
 
 from jinja2 import Environment, PackageLoader, select_autoescape  # type:ignore
 from storylc.model import Movie, Scene
@@ -16,6 +17,18 @@ def get_old(outfile: Path):
             return ""
 
 
+def make_starts(movie: Movie) -> List[int]:
+    def rec_f(scenes: List[Scene], starts: List[int]) -> List[int]:
+        if scenes == []:
+            return starts
+        scene = scenes[0]
+        scenes = scenes[1:]
+        starts = starts + [starts[-1] + scene.duration * scene.ips]
+        return rec_f(scenes, starts)
+
+    return rec_f(movie.scenes, [0])
+
+
 def generate_omakeroot(movie: Movie, out: Path) -> bool:
     j_file: Path = here / "OMakeroot.jinja"
     outfile = out / "OMakeroot"
@@ -30,9 +43,11 @@ def generate_omakeroot(movie: Movie, out: Path) -> bool:
     a_logger.info(f"{str(outfile.absolute())} was regenerated")
     return False
 
-def generate_scene(movie:Movie,scene:Scene,out:Path):
+
+def generate_scene(movie: Movie, scene: Scene, out: Path):
     j_file: Path = here / "scene.mp.jinja"
-    outfile = out / f"g_{scene.path}"
+    outfile = out / f"tmp-{scene.name}/{scene.path}"
+    (outfile.parent).mkdir(exist_ok=True)
     env: Environment = Environment()
     template = env.from_string(source=j_file.read_text(), globals={})
     old_data = get_old(outfile)
@@ -45,6 +60,7 @@ def generate_scene(movie:Movie,scene:Scene,out:Path):
     a_logger.info(f"{str(outfile.absolute())} was regenerated")
     return False
 
+
 def generate_omakefile(movie: Movie, out: Path) -> bool:
     j_file: Path = here / "OMakefile.jinja"
     outfile = out / "OMakefile"
@@ -52,13 +68,30 @@ def generate_omakefile(movie: Movie, out: Path) -> bool:
     template = env.from_string(source=j_file.read_text(), globals={})
     old_data = get_old(outfile)
     a_logger.info(movie.scenes)
-    new_data = template.render(movie=movie, scenes=movie.scenes)
+    starts = make_starts(movie)
+    nb_images=starts[-1]
+    new_data = template.render(movie=movie, scenes=movie.scenes,zip=zip(movie.scenes,starts),nb_images=nb_images)
     if old_data == new_data:
         a_logger.info(f"{str(outfile.absolute())} was not regenerated")
         return True
     outfile.write_text(data=new_data)
     a_logger.info(f"{str(outfile.absolute())} was regenerated")
     return False
+
+def generate_omakefile_scene(scene: Scene, out: Path) -> bool:
+    j_file: Path = here / "OMakefile_scene.jinja"
+    outfile = out / f"tmp-{scene.name}/OMakefile"
+    env: Environment = Environment()
+    template = env.from_string(source=j_file.read_text(), globals={})
+    old_data = get_old(outfile)
+    new_data = template.render(scene=scene)
+    if old_data == new_data:
+        a_logger.info(f"{str(outfile.absolute())} was not regenerated")
+        return True
+    outfile.write_text(data=new_data)
+    a_logger.info(f"{str(outfile.absolute())} was regenerated")
+    return False
+
 
 def generate_master_tex(movie: Movie, out: Path) -> bool:
     j_file: Path = here / "master.tex.jinja"
@@ -67,18 +100,11 @@ def generate_master_tex(movie: Movie, out: Path) -> bool:
     template = env.from_string(source=j_file.read_text(), globals={})
     old_data = get_old(outfile)
     a_logger.info(movie.scenes)
-    def make_starts() :
-        def rec_f(scenes,starts):
-            if scenes==[]:
-                return starts
-            scene=scenes[0]
-            scenes=scenes[1:]
-            starts = starts + [ starts[-1]+scene.duration*scene.ips]
-            return rec_f(scenes,starts)
-        return rec_f(movie.scenes,[0])
-    starts=make_starts()
-    assert(len(starts)==len(movie.scenes)+1)
-    new_data = template.render(zip=zip(movie.scenes,starts))
+
+    starts = make_starts(movie)
+    a_logger.info(starts)
+    assert len(starts) == len(movie.scenes) + 1
+    new_data = template.render(zip=zip(movie.scenes, starts))
 
     if old_data == new_data:
         a_logger.info(f"{str(outfile.absolute())} was not regenerated")
@@ -86,6 +112,7 @@ def generate_master_tex(movie: Movie, out: Path) -> bool:
     outfile.write_text(data=new_data)
     a_logger.info(f"{str(outfile.absolute())} was regenerated")
     return False
+
 
 def generate_timeline(movie: Movie, out: Path) -> bool:
     j_file: Path = here / "timeline.jinja"
@@ -94,8 +121,9 @@ def generate_timeline(movie: Movie, out: Path) -> bool:
     template = env.from_string(source=j_file.read_text(), globals={})
     old_data = get_old(outfile)
     a_logger.info(movie.scenes)
-    nb_points = reduce(lambda a,b:a+b,list(map(lambda scene:scene.duration*scene.ips,movie.scenes)),0)
-    new_data = template.render(nb_points=nb_points)
+    starts = make_starts(movie)
+    nb_images=starts[-1]
+    new_data = template.render(nb_images=nb_images)
     if old_data == new_data:
         a_logger.info(f"{str(outfile.absolute())} was not regenerated")
         return True
@@ -103,29 +131,29 @@ def generate_timeline(movie: Movie, out: Path) -> bool:
     a_logger.info(f"{str(outfile.absolute())} was regenerated")
     return False
 
-def copy_mp(out:Path):
-    from_paths = [
-        "storylc/lib/slide.mp"
-    ]
-    def copy_one(source:str):
-        source_path: Path=here.parent/source
+
+def copy_mp(out: Path):
+    from_paths = ["storylc/lib/slide.mp"]
+
+    def copy_one(source: str):
+        source_path: Path = here.parent / source
         a_logger.info(f"copy {str(source_path.absolute())}")
-        target_path:Path=out/source
+        target_path: Path = out / source
         target_path.write_text(source_path.read_text())
 
-    (out/"storylc/lib").mkdir(exist_ok=True,parents=True)
-    list(map(lambda source:copy_one(source),from_paths))
+    (out / "storylc/lib").mkdir(exist_ok=True, parents=True)
+    list(map(lambda source: copy_one(source), from_paths))
 
-def copy_src(movie:Movie,out:Path):
-    def copy_one(scene:Scene):
-        source_path:Path= movie.root/scene.path
+
+def copy_src(movie: Movie, out: Path):
+    def copy_one(scene: Scene):
+        source_path: Path = movie.root / scene.path
         a_logger.info(f"copy {str(source_path.absolute())}")
-        target_path:Path=out/"mounted"/scene.path
+        target_path: Path = out / "mounted" / scene.path
         target_path.write_text(source_path.read_text())
 
-    (out/"mounted").mkdir(exist_ok=True,parents=True)
-    list(map(lambda scene:copy_one(scene=scene),movie.scenes))
-
+    (out / "mounted").mkdir(exist_ok=True, parents=True)
+    list(map(lambda scene: copy_one(scene=scene), movie.scenes))
 
 
 def generate(movie: Movie, out: Path):
@@ -134,6 +162,17 @@ def generate(movie: Movie, out: Path):
     generate_omakefile(movie=movie, out=out)
     generate_master_tex(movie=movie, out=out)
     generate_timeline(movie=movie, out=out)
-    list(map(lambda scene:generate_scene(movie=movie,scene=scene,out=out),movie.scenes))
+    list(
+        map(
+            lambda scene: generate_scene(movie=movie, scene=scene, out=out),
+            movie.scenes,
+        )
+    )
+    list(
+        map(
+            lambda scene: generate_omakefile_scene(scene=scene, out=out),
+            movie.scenes,
+        )
+    )
     copy_mp(out)
-    copy_src(movie=movie,out=out)
+    copy_src(movie=movie, out=out)
